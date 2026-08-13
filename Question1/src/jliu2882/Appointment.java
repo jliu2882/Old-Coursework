6def9431de0ba9109d6b566155512ab2c98164bc
BuildTools <unconfigured@null.spigotmc.org>
2026-08-13 17:24:14 -0400

    combine repositories

---
+++ Question1/src/jliu2882/Appointment.java
@@ -0,0 +1,19 @@
+package jliu2882;
+
+public class Appointment {
+    public TimeInterval interval;
+
+    public Appointment(TimeInterval interval){
+        this.interval = interval;
+    }
+
+    public TimeInterval getTime(){
+        return interval;
+    }
+    public boolean conflictsWith(Appointment other){
+        if(this.interval.overlapsWith(other.getTime())){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Question1/src/jliu2882/TimeInterval.java
@@ -0,0 +1,19 @@
+package jliu2882;
+
+public class TimeInterval {
+    public int start;
+    public int end;
+
+    public TimeInterval(int startTime, int endTime) {
+        this.start = startTime;
+        this.end = endTime;
+    }
+
+    public boolean overlapsWith (TimeInterval interval){
+        if(this.start>=interval.start && this.start<=interval.end ||
+                this.end>=interval.start && this.end<=interval.end){
+            return true;
+        }
+        return false;
+    }
+}
